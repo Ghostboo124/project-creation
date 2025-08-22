607987a9d6c45267eff9ceed8d317ecb04d02883
Ghostboo124 <alexander.perkins11@gmail.com>
2025-08-22 18:17:00 +1000

    Initial Commit
    Signed-off-by: Ghostboo124 <alexander.perkins11@gmail.com>

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project Creation
+
+My TUI for generating different types of projects (python, uv python, rust, cmake cpp are the goals)
---
+++ src/main.rs
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) Alexander Perkins, 2025
+ *
+ * This work is free.  You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To But It's Not My Fault Public
+ * License, Version 1, as published by Ben McGinnes.  See the
+ * COPYING.WTFNMFPLv1a.txt file for more details.
+ */
+
+fn main() {
+    println!("Hello, world!");
+}
